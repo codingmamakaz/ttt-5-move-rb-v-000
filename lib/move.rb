c561c3cd889f847ac64b409f161bd0b7192152ce
Kazumi Karbowski <kazkazinus@gmail.com>
2018-03-20 01:08:24 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -23,3 +23,12 @@ def position_taken?(board, index)
     true
   end
 end
+
+def valid_move?(board, index)
+  if index.between?(0, 8) && !position_taken?(board, index)
+    true
+
+  else
+    false
+  end
+end
